62ae9e04c02ac47370d7e07c2bfaf0b8ee81539b
Matt Murphy <mmurphydev@gmail.com>
2023-09-26 12:29:08 -0400

    add theme and fonts

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,19 +1,44 @@
 import React from 'react'
 import styled from 'styled-components/native'
 import { Card } from 'react-native-paper'
+import { SvgXml } from 'react-native-svg'
+import star from '../../../../assets/star.js'
+import open from '../../../../assets/open.js'
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
-  background-color: white;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
 `
 
 const Title = styled.Text`
-  padding: 16px;
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
 `
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`
+
+const Section = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = 'Some Restaurant',
@@ -26,10 +51,26 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily = false,
   } = restaurant
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)))
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map((item, i) => {
+              return <SvgXml xml={star} width={20} height={20} key={i} />
+            })}
+          </Rating>
+          {isOpenNow && !isClosedTemporarily && (
+            <SvgXml xml={open} width={20} height={20} />
+          )}
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   )
 }
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -10,12 +10,12 @@ const Container = styled(SafeAreaView)`
 `
 
 const Search = styled(View)`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `
 
 const List = styled(View)`
   flex: 1;
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `
 
 export const RestaurantsScreen = () => {
