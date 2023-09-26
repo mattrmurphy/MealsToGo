90851c4ec3f67070dbc693c8c1681f05f4adf5d6
Matt Murphy <mmurphydev@gmail.com>
2023-09-26 13:01:43 -0400

    add spacer component

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Text, Image } from 'react-native'
 import styled from 'styled-components/native'
 import { Card } from 'react-native-paper'
 import { SvgXml } from 'react-native-svg'
+
+import { Spacer } from '../../../components/spacer/spacer.component'
 import star from '../../../../assets/star.js'
 import open from '../../../../assets/open.js'
 
@@ -39,17 +42,23 @@ const Section = styled.View`
   align-items: center;
 `
 
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = 'Some Restaurant',
-    icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = [
       'https://a.cdn-hotels.com/gdcs/production0/d1513/35c1c89e-408c-4449-9abe-f109068f40c0.jpg?impolicy=fcrop&w=1600&h=1066&q=medium',
     ],
     address = '123 Main St.',
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant
 
   const ratingArray = Array.from(new Array(Math.floor(rating)))
@@ -65,9 +74,18 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
               return <SvgXml xml={star} width={20} height={20} key={i} />
             })}
           </Rating>
-          {isOpenNow && !isClosedTemporarily && (
-            <SvgXml xml={open} width={20} height={20} />
-          )}
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: 'red' }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer position="left" size="large" />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
         </Section>
         <Address>{address}</Address>
       </Info>
