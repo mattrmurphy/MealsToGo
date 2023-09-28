23c93fe7ab8ba4b447c06c55958386852dd83dcc
Matt Murphy <mmurphydev@gmail.com>
2023-09-28 11:06:43 -0400

    remove unused param

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -14,7 +14,7 @@ export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
+    restaurant.photos = restaurant.photos.map(() => {
       return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))]
     })
     return {
